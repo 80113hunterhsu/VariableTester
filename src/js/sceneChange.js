f5ebd3765bf3965f9bacfc1388744b3ea6339a07
Hunter Hsu <80113hunterhsu@gmail.com>
2022-03-01 22:58:56 +0800

    develop in progress
    1. video view is done, but still need level measurement buttons.
    2. currently done at video viewing, still need result output and other stuff

---
+++ src/js/sceneChange.js
@@ -0,0 +1,6 @@
+﻿//a global scene changer for variable tester
+
+function sceneChange(sceneID1, sceneID2) {
+    document.getElementById(sceneID1).style.display = "none";
+    document.getElementById(sceneID2).style.display = "block";
+}
