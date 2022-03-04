6e200d093f24124a3fbd4061bb2e4b50b46dd5cf
Hunter Hsu <80113hunterhsu@gmail.com>
2022-03-04 22:08:55 +0800

    scoreboard in progress
    score bar needed
    file(csv, graph) output needed

--- src/js/timestampRecord.js
+++ src/js/timestampRecord.js
@@ -5,3 +5,45 @@ function getCurrentPlayTime() {
     var time_array = time.split(":");
     var seconds = (parseInt(time_array[0], 10) * 60) + (parseInt(time_array[1], 10));
 }
+
+function scoreUpdate() {
+    if (score > 5) {
+        score = 5;
+    }
+    else if (score < -5) {
+        score = -5;
+    }
+    scoreboard.innerHTML = score;
+}
+
+function keyScoring(key) {
+    //console.log("key: " + key.code);
+    if ((key.code === 'KeyV') || (key.code === 'KeyN')) {
+        scoring(key.code);
+    }
+    if (key.code === 'Space') {
+        //
+    }
+}
+
+function scoring(key) {
+    if (score <= 5 && score >= -5) {
+        if (key === 'KeyV') {
+            score--;
+        }
+        if (key === 'KeyN') {
+            score++;
+        }
+    }
+    //console.log(score);
+}
+
+function resetScoreToZero() {
+    if (score > 0) {
+        score--;
+    }
+    else if (score < 0) {
+        score++;
+    }
+    //console.log(score);
+}
