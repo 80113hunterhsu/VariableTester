535a9f21c5b2cd46a51f198fe341e62fcef0c647
Hunter Hsu <80113hunterhsu@gmail.com>
2022-03-12 22:39:22 +0800

    fixed jscdn of youtube2html5

---
+++ src/js/webhostDisclaimerHide.js
@@ -0,0 +1,20 @@
+﻿// JavaScript Document
+var i = 0;
+function hide() {
+    document.querySelector(".disclaimer").style.display = "none";
+    i++;
+}
+
+function stop() {
+    if (i >= 100) {
+        clearInterval(run);
+    }
+}
+
+function runner() {
+    hide();
+    stop();
+}
+
+var run = setInterval(runner, 100);
+
