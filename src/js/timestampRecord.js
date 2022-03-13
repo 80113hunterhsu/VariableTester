0994c601f21253735201ede55ebc4adf77605191
Hunter Hsu <80113hunterhsu@gmail.com>
2022-03-13 22:31:22 +0800

    added keystroke display, limited video format to mp4
    scoreBar still not started yet, planning on using canvas

--- src/js/timestampRecord.js
+++ src/js/timestampRecord.js
@@ -11,10 +11,22 @@ function scoreUpdate() {
     scoreRecord();
 }
 
+function keyClicked(key) {
+    if (key === 'KeyV') {
+        document.querySelector("#scoreMinus").style.backgroundColor = "#1c7530";
+        setTimeout(function () {document.querySelector("#scoreMinus").style.backgroundColor = "#28a745";}, 100);
+    }
+    else if (key === 'KeyN') {
+        document.querySelector("#scorePlus").style.backgroundColor = "#a81d2a";
+        setTimeout(function () {document.querySelector("#scorePlus").style.backgroundColor = "#dc3545";}, 100);
+    }
+}
+
 function keyScoring(key) {
     //console.log("key: " + key.code);
     if ((key.code === 'KeyV') || (key.code === 'KeyN')) {
         scoring(key.code);
+        keyClicked(key.code);
     }
     if (key.code === 'Space') {
         //
