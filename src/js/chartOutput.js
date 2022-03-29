7e96ea3fb319cfc017a7f62bfd816500f34c02b0
Hunter Hsu <80113hunterhsu@gmail.com>
2022-03-29 15:23:35 +0800

    fixed chart
    density problem: dynamic canvas width
    label: sec to min.sec

--- src/js/chartOutput.js
+++ src/js/chartOutput.js
@@ -1,30 +1,75 @@
 ﻿//line chart output
 var scoreTime = [0];
 var scoreScore = [0];
+//test
+/*var testScore = 5;
+var teststat = -1;
+for (var i = 0; i < 400; i++) {
+    scoreTime[i] = i;
+    if (teststat = -1) {
+        scoreScore[i] = testScore--;
+        if (testScore <= -5) {
+            teststat = 1;
+            testScore = -4;
+        }
+    }
+    if (teststat == 1) {
+        scoreScore[i] = testScore++;
+        if (testScore >= 5) {
+            teststat = -1;
+            testScore = 4;
+        }
+    }
+}*/
+
 function chartPreprocess() {
     for (var i = 0; i <= curSec; i++) {
         scoreTime[i] = scoreArr[i][0];
         scoreScore[i] = scoreArr[i][1];
+    }
+    secondToMS();
+}
+
+var scoreTimeMS = ["0分0秒"];
+function secondToMS() {
+    //curSec = 399;
+    for (var i = 1; i <= curSec; i++) {
+        var tmp = (parseInt(scoreTime[i] / 60)) + "分" + (scoreTime[i] % 60) + "秒";
+        scoreTimeMS.push(tmp);
+        console.log(tmp);
     }
 }
 
 //Chart.JS initialization
 const resultChart = document.getElementById('resultChart');
 function initChart() {
+    if (curSec < 10) {
+        resultChart.style.width = curSec * 80 + "px";
+    }
+    else if (curSec < 20) {
+        resultChart.style.width = curSec * 50 + "px";
+    }
+    else if (curSec < 60) {
+        resultChart.style.width = curSec * 40 + "px";
+    }
+    else if (curSec >= 60) {
+        resultChart.style.width = curSec * 30 + "px";
+    }
     const myChart = new Chart(resultChart, {
         type: 'line',
         data: {
-            labels: scoreTime,
+            labels: scoreTimeMS,
             datasets: [{
                 label: var1,
                 data: scoreScore,
                 backgroundColor: '#28a745',
                 borderColor: '#28a745',
-                borderWidth: 1
+                borderWidth: 1, 
+                pointRadius: 1.5
             }]
         },
         options: {
-            responsive: true,
+            responsive: false,
             scales: {
                 y: {
                     beginAtZero: true, 
@@ -40,6 +85,9 @@ function initChart() {
                     title: {
                       display: true,
                       text: 'Time'
+                    }, 
+                    ticks: {
+                        autoSkip: false
                     }
                 }
             }
